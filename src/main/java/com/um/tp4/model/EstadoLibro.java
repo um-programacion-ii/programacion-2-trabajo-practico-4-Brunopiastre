ecebb8b18fa95586cf1e31171da5fe70908bfcd3
Brunopiastre <b.piastrellini@alumno.um.edu.ar>
2025-05-09 20:06:58 -0300

    Etapa 1: modelos base implementados (Libro, Usuario, Prestamo)

---
+++ src/main/java/com/um/tp4/model/EstadoLibro.java
@@ -0,0 +1,7 @@
+package com.um.tp4.model;
+
+public enum EstadoLibro {
+    DISPONIBLE,
+    PRESTADO,
+    EN_REPARACION
+}
---
+++ src/main/java/com/um/tp4/model/EstadoUsuario.java
@@ -0,0 +1,7 @@
+package com.um.tp4.model;
+
+public enum EstadoUsuario {
+    ACTIVO,
+    SUSPENDIDO,
+    MOROSO
+}
